207c926afa99a1d91e7a34a9f91ae78bfcd1fdf9
Douglas Hoang <douglas.hoang@4doutlets.com>
2022-02-17 10:52:23 -0600

    adds readme

--- src/lib.rs
+++ src/lib.rs
@@ -199,35 +199,35 @@ pub mod css {
         a: u8,
     }
 
-    fn parse_simple_selector(&mut self) -> SimpleSelector {
-        let mut selector = SimpleSelector {
-            tag_name: None,
-            id: None,
-            class: Vec::new(),
-        };
+    // fn parse_simple_selector(&mut self) -> SimpleSelector {
+    //     let mut selector = SimpleSelector {
+    //         tag_name: None,
+    //         id: None,
+    //         class: Vec::new(),
+    //     };
 
-        while !self.eof() {
-            match self.next_char() {
-                '#' => {
-                    self.consume_char();
-                    selector.id = Some(self.parse_identifier());
-                }
-                '.' => {
-                    self.consume_char();
-                    selector.class.push(self.parse_identifier());
-                }
-                '*' => {
-                    self.consume_char();
-                }
-                c if valid_identifier_char(c) => {
-                    selector.tag_name = Some(self.parse_identifier());
-                }
-                _ => break,
-            }
-        }
+    //     while !self.eof() {
+    //         match self.next_char() {
+    //             '#' => {
+    //                 self.consume_char();
+    //                 selector.id = Some(self.parse_identifier());
+    //             }
+    //             '.' => {
+    //                 self.consume_char();
+    //                 selector.class.push(self.parse_identifier());
+    //             }
+    //             '*' => {
+    //                 self.consume_char();
+    //             }
+    //             c if valid_identifier_char(c) => {
+    //                 selector.tag_name = Some(self.parse_identifier());
+    //             }
+    //             _ => break,
+    //         }
+    //     }
 
-        return selector;
-    }
+    //     return selector;
+    // }
 
     pub type Specificity = (usize, usize, usize);
 
